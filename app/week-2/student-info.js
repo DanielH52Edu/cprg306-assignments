e600d2deda8a04faf6073a6db97a9016bcffb31e
DanielH52 <180554429+DanielH52Edu@users.noreply.github.com>
2024-09-24 17:13:58 -0600

    Upload week 2 assignment.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function StudentInfo () {
+    return <section>
+        <h1>Daniel Hockridge</h1>
+        <Link href="https://github.com/DanielH52Edu/cprg306-assignments">https://github.com</Link>
+    </section>
+}
