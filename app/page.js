2fcb529e1a747239e37d5c49d9670a21d1269f0c
DanielH52Edu <180554429+DanielH52Edu@users.noreply.github.com>
2024-10-01 21:23:17 -0600

    Upload week 3 assignment.

--- app/page.js
+++ app/page.js
@@ -5,9 +5,10 @@ export default function Home() {
   return (
     <main className={'flex flex-col items-center justify-center'}>
       <h1 className={'text-2xl m-6'}>CPRG 306: Web Development 2 - Assignments</h1>
-      <ul>
-        <li><Link href={'week-2'}>Week 2</Link></li>
-      </ul>
+        <ul>
+            <li><Link href={'week-2'}>Week 2</Link></li>
+            <li><Link href={'week-3'}>Week 3</Link></li>
+        </ul>
     </main>
   );
 }
---
+++ app/week-3/page.js
@@ -0,0 +1,11 @@
+import ItemList from "@/app/week-3/item-list";
+
+
+export default function Page() {
+    return (
+        <main>
+            <h1 className="text-3xl font-bold mb-6">Shopping List</h1>
+            <ItemList />
+        </main>
+    )
+}
