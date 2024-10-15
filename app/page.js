a127c2e6d3942d4710f2b4d1525e767798e5c450
DanielH52Edu <180554429+DanielH52Edu@users.noreply.github.com>
2024-10-15 08:31:18 -0600

    Add week 5

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
             <li><Link href={'week-2'} className="hover:underline hover:text-green-200">Week 2</Link></li>
             <li><Link href={'week-3'} className="hover:underline hover:text-green-200">Week 3</Link></li>
             <li><Link href={'week-4'} className="hover:underline hover:text-green-200">Week 4</Link></li>
+            <li><Link href={'week-5'} className="hover:underline hover:text-green-200">Week 5</Link></li>
         </ul>
     </main>
   );
---
+++ app/week-5/page.js
@@ -0,0 +1,9 @@
+import NewItem from "@/app/week-5/new-item";
+
+export default function Page() {
+    return (
+        <main className="mt-3 flex justify-center">
+            <NewItem />
+        </main>
+    )
+}
